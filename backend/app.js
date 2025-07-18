4b66852bca9fe85af51c44f1f4f8cfab00ca3935
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-07-18 11:10:49 +0200

    add: .json and urlencoded methods

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(cors({ origin: "http//:localhost:3000" }));
 app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 helmet.contentSecurityPolicy({
   directives: {
