4ba0623cadca9f8bc2c1b2cd082366e0d3bb9fc3
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-07-20 12:30:00 +0200

    add: utils module

---
+++ backend/utils/utils.js
@@ -0,0 +1,44 @@
+const { jwtDecode } = require("jwt-decode");
+const bcrypt = require("bcrypt");
+const { config } = require("dotenv");
+const jsonwebtoken = require("jsonwebtoken");
+
+config();
+
+const singToken = async (credentials) => {
+  const payload = {
+    email: credentials.email,
+    role: credentials.role,
+  };
+  return jsonwebtoken.sign(payload, process.env.JWT_SECRET, {
+    algorithm: "HS256",
+    expiresIn: "1h",
+  });
+};
+
+const decodeToken = (token) => {
+  return jwtDecode(token);
+};
+
+const createHashedPassword = async (password) => {
+  return await new Promise((resolve, reject) => {
+    bcrypt.hash(password, 12, (err, hashedPassword) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      resolve(hashedPassword);
+    });
+  });
+};
+
+const verifyPassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+module.exports = {
+  decodeToken,
+  createHashedPassword,
+  verifyPassword,
+  singToken,
+};
