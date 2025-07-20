4c0992878ab1b021b43cf216958bdfdafa0fb291
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-07-20 12:31:44 +0200

    add: userController with logInUser and signUpUser middlewares

---
+++ backend/controllers/userController.js
@@ -0,0 +1,109 @@
+const { body, validationResult } = require("express-validator");
+const db = require("../db/queries");
+const {
+  verifyPassword,
+  createHashedPassword,
+  decodeToken,
+  singToken,
+} = require("../utils/utils");
+
+const validateUser = [
+  body("email", "Invalid email address.")
+    .isEmail()
+    .custom(async (value) => {
+      const user = await db.findUser(value);
+      if (user) throw new Error("User already exists");
+    }),
+  body(
+    "password",
+    "Password must be at least 8 characters long. And it must contain at least one lowercase letter, one uppercase one symbol and one number."
+  ).isStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  }),
+  body("confirm_password", "Passwords must match.").custom((value, { req }) => {
+    return value === req.body.password;
+  }),
+];
+const signUpUser = [
+  validateUser,
+  async (req, res, next) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json([errors]);
+    }
+
+    const hashedPassword = await createHashedPassword(password);
+    try {
+      const user = await db.createUser(email, hashedPassword);
+      const token = singToken(user);
+      const decodedToken = decodeToken(token);
+      const expiresAt = decodedToken.exp;
+
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
+
+      const { email, role } = user;
+
+      const userInfo = {
+        email,
+        role,
+      };
+      return res.json({
+        message: "User Created!",
+        token,
+        userInfo,
+        expiresAt,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        message: "There was a problem with creating your account",
+      });
+    }
+  },
+];
+
+const logInUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await db.findUser(email);
+  if (!user)
+    return res.status(403).json({
+      message: "Wrong email or password",
+    });
+
+  const match = verifyPassword(password, user.password);
+
+  if (match) {
+    const { ...rest } = user;
+
+    const userInfo = Object.assign({}, { ...rest });
+
+    const token = await singToken(userInfo);
+    const decodedToken = decodeToken(token);
+    const expiresAt = decodedToken.exp;
+
+    res.cookie("token", token, {
+      httpOnly: true,
+    });
+
+    return res.json({
+      message: "Authentication successful",
+      token,
+      userInfo,
+      expiresAt,
+    });
+  }
+};
+
+module.exports = {
+  signUpUser,
+  logInUser,
+};
