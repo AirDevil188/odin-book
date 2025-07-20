b6f162350839910439e7b0b82b7b00e64422b8c1
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-07-20 22:04:42 +0200

    add: databaseUrl conditional PrismaClient connection to db based on dbUrl

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -1,6 +1,17 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const databaseUrl =
+  process.env.NODE_ENV === "TEST"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
 
 const findUser = async (email) => {
   return await prisma.user.findUnique({
