9347d3390f1303544fe44310aa8ced325d9e77dd
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-07-20 12:33:34 +0200

    intiliazed attachUser middleware, cookieParser and userRouter

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const { config } = require("dotenv");
+const cookieParser = require("cookie-parser");
 config();
 
 const port = process.env.PORT;
 
 const app = express();
 
+const userRouter = require("./routes/userRouter");
+const { attachUser } = require("./middlewares/authorization");
+
 app.use(cors({ origin: "http//:localhost:3000" }));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(attachUser);
+
+app.use("/", userRouter);
 
 helmet.contentSecurityPolicy({
   directives: {
@@ -20,7 +28,6 @@ helmet.contentSecurityPolicy({
     fontSrc: ["'self'", "https://fonts.gstatic.com"],
   },
 });
-
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send(err);
