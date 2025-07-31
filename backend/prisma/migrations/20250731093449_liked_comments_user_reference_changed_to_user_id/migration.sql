cc5dde1aec0c314b193c533ccc974ad70897da6e
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-07-31 11:36:54 +0200

    likedComments  db model onDelete Cascade, changed profile  relation reference to userId

---
+++ backend/prisma/migrations/20250731093449_liked_comments_user_reference_changed_to_user_id/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "liked_comments" DROP CONSTRAINT "liked_comments_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "liked_comments" ADD CONSTRAINT "liked_comments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "profiles"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
