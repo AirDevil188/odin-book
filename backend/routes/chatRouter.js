1f9eb54cf298a44951d4cb18562ad1e19cf67faf
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-08-11 22:38:23 +0200

    add: createChat route

--- backend/routes/chatRouter.js
+++ backend/routes/chatRouter.js
@@ -9,6 +9,8 @@ chatRouter.get("/", requireAuth, chatController.getChats);
 
 chatRouter.get("/:chatroomId", requireAuth, chatController.getChat);
 
+chatRouter.post("/:userId2", requireAuth, chatController.createChat);
+
 chatRouter.put("/:chatroomId/delete", requireAuth, chatController.deleteChat);
 
 module.exports = chatRouter;
