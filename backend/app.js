c8b042cb67b93558da2f8614bac5616c077650be
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-08-27 13:16:58 +0200

    fix: add parent apiRouter

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ config();
 const port = process.env.PORT;
 
 const app = express();
+const apiRouter = express.Router();
 
 const userRouter = require("./routes/userRouter");
 const adminRouter = require("./routes/adminRouter");
@@ -18,20 +19,33 @@ const messageRouter = require("./routes/messageRouter");
 const chatRouter = require("./routes/chatRouter");
 const groupRouter = require("./routes/groupRouter");
 
-app.use(cors({ origin: "http//:localhost:3000" }));
+const corsOptions = ["http://localhost:5173"];
+
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/", userRouter);
-app.use("/admin", adminRouter);
-app.use("/profiles", profileRouter);
-app.use("/profiles/posts", postRouter);
-app.use("/profiles/comments", commentRouter);
-app.use("/profiles/messages", messageRouter);
-app.use("/profiles/chats", chatRouter);
-app.use("/profiles/groups", groupRouter);
+apiRouter.use("/", userRouter);
+apiRouter.use("/admin", adminRouter);
+apiRouter.use("/profiles", profileRouter);
+apiRouter.use("/profiles/posts", postRouter);
+apiRouter.use("/profiles/comments", commentRouter);
+apiRouter.use("/profiles/messages", messageRouter);
+apiRouter.use("/profiles/chats", chatRouter);
+apiRouter.use("/profiles/groups", groupRouter);
+
+// app.use("/api", userRouter);
+// app.use("/admin", adminRouter);
+// app.use("/profiles", profileRouter);
+// app.use("/profiles/posts", postRouter);
+// app.use("/profiles/comments", commentRouter);
+// app.use("/profiles/messages", messageRouter);
+// app.use("/profiles/chats", chatRouter);
+// app.use("/profiles/groups", groupRouter);
+
+app.use("/api", apiRouter);
 
 helmet.contentSecurityPolicy({
   directives: {
@@ -40,6 +54,7 @@ helmet.contentSecurityPolicy({
     fontSrc: ["'self'", "https://fonts.gstatic.com"],
   },
 });
+
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send(err);
