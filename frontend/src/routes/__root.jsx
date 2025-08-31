3bf87e6d58facd83778ba15552b58b655781def8
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-08-31 20:16:52 +0200

    add: root route for TanStack router

--- frontend/src/routes/__root.jsx
+++ frontend/src/routes/__root.jsx
@@ -1,12 +1,17 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import ErrorBoundary from "../components/Error Boundary/ErrorBoundary";
+import { ToastContainer } from "react-toastify";
 
 export const Route = createRootRoute({
   component: () => {
     return (
       <>
-        <Outlet />
-        <TanStackRouterDevtools />
+        <ErrorBoundary>
+          <Outlet />
+          <TanStackRouterDevtools />
+          <ToastContainer />
+        </ErrorBoundary>
       </>
     );
   },
