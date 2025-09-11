d68dd57a4fa131df17e5a2c80877cbc53c6e0f75
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-11 12:24:34 +0200

    fix: refactored postLogin to use axios

--- frontend/src/api/postLogin.js
+++ frontend/src/api/postLogin.js
@@ -1,15 +1,7 @@
+import axios from "../api/axiosInstance";
+
 export default async function postLogin(email, password) {
-  const response = await fetch("/api/log-in", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, password }),
-  });
+  const response = await axios.post("/log-in", { email, password });
 
-  if (!response.ok) {
-    throw new Error("Response error");
-  }
-
-  return await response.json();
+  return await response.data;
 }
