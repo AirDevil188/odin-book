26ed37e05fee441f2063c91b43b6b0508313a425
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-11 11:02:54 +0200

    add: createHash, verifyHash, generateRawToken, genrateSelectorToken, newDateOneWeek

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -2,6 +2,8 @@ const { jwtDecode } = require("jwt-decode");
 const bcrypt = require("bcrypt");
 const { config } = require("dotenv");
 const jsonwebtoken = require("jsonwebtoken");
+const randToken = require("rand-token");
+const { addWeeks } = require("date-fns");
 
 config();
 
@@ -13,7 +15,7 @@ const singToken = async (credentials) => {
   };
   return jsonwebtoken.sign(payload, process.env.JWT_SECRET, {
     algorithm: "HS256",
-    expiresIn: "1h",
+    expiresIn: "15min",
   });
 };
 
@@ -21,25 +23,40 @@ const decodeToken = (token) => {
   return jwtDecode(token);
 };
 
-const createHashedPassword = async (password) => {
-  return await new Promise((resolve, reject) => {
-    bcrypt.hash(password, 12, (err, hashedPassword) => {
-      if (err) {
-        console.log(err);
-        reject(err);
-      }
-      resolve(hashedPassword);
-    });
-  });
+const verifyHash = async (value, hashedValue) => {
+  return await bcrypt.compare(value, hashedValue);
 };
 
-const verifyPassword = async (password, hashedPassword) => {
-  return await bcrypt.compare(password, hashedPassword);
+const generateRawToken = () => {
+  return randToken.uid(64);
+};
+
+const generateSelectorToken = () => {
+  return randToken.uid(18);
+};
+
+const createHash = async (value) => {
+  try {
+    const hashedToken = await bcrypt.hash(value, 12);
+    return hashedToken;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const newDateOneWeek = (currentDate) => {
+  const oneWeekDate = addWeeks(currentDate, 1);
+
+  return oneWeekDate;
 };
 
 module.exports = {
   decodeToken,
-  createHashedPassword,
-  verifyPassword,
+  createHash,
+  verifyHash,
   singToken,
+  generateRawToken,
+  generateSelectorToken,
+  newDateOneWeek,
 };
