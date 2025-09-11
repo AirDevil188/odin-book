8d6c79538ebf21efb91f92af279b53939fa77e7d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-11 11:41:10 +0200

    add: AuthContext provider

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,52 @@
+import { useState, createContext, useEffect, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const userInfo = localStorage.getItem("userInfo");
+
+  const [authState, setAuthState] = useState({
+    accessToken: null,
+    userInfo: userInfo ? JSON.stringify(userInfo) : {},
+  });
+
+  const setAuthInfo = ({ accessToken, userInfo, expiresAt }) => {
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+
+    return setAuthState({ accessToken, userInfo, expiresAt });
+  };
+
+  useEffect(() => {});
+
+  const logout = () => {
+    // remove userInfo from the local storage
+    localStorage.removeItem("userInfo");
+    // delete token from the state
+    setAuthState((prevState) => ({
+      ...prevState,
+      accessToken: null,
+      userInfo: {},
+    }));
+  };
+
+  const isAuthenticated = () => {
+    if (!authState.accessToken || !authState.expiresAt) return false;
+    // return true if current time is less than expires on the token
+    else return new Date().getTime() / 1000 < authState.expiresAt;
+  };
+
+  const contextValue = {
+    authState,
+    setAuthState: (authInfo) => setAuthInfo(authInfo),
+    isAuthenticated,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
