2f0962f7f34dadbac19bf5e9526f6f5cf07b55b1
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-11 10:59:18 +0200

    fix: changed requireAuth middleware to check for the token in the authorization bearer header

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -7,7 +7,16 @@ config();
 const requireAuth = jwt({
   secret: process.env.JWT_SECRET,
   algorithms: ["HS256"],
-  getToken: (req) => req.cookies.token,
+  getToken: (req) => {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Bearer"
+    ) {
+      return req.headers.authorization.split(" ")[1];
+    }
+
+    return null;
+  },
   requestProperty: "user",
 });
 
