9bb1f0f12dccd9bb5ee8ce9e6e4b3ba622a8d31c
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-18 00:22:06 +0200

    fix: AxiosInterceptor add _intercepted flag to avoid infinite loop

--- frontend/src/context/AxiosInterceptor.jsx
+++ frontend/src/context/AxiosInterceptor.jsx
@@ -1,15 +1,16 @@
+// AxiosInterceptor.jsx
 import { useEffect } from "react";
 import { useAuth } from "./AuthContext";
 import axios from "../api/axiosInstance";
-
-// let isRefreshing = false;
 
 const AxiosInterceptor = ({ children }) => {
   const authContext = useAuth();
 
   useEffect(() => {
+    // request interceptor
     const requestInterceptor = axios.interceptors.request.use(
       (config) => {
+        // if accessToken is present put it in the Bearer auth header
         if (authContext.authState.accessToken) {
           config.headers.Authorization = `Bearer ${authContext.authState.accessToken}`;
         }
@@ -17,37 +18,38 @@ const AxiosInterceptor = ({ children }) => {
       },
       (error) => Promise.reject(error),
     );
+    // response interceptor
     const responseInterceptor = axios.interceptors.response.use(
       (response) => response,
       async (error) => {
         const originalRequest = error.config;
+        // if the error came from the /token/refresh endpoint reject it
+        if (originalRequest._intercepted) {
+          return Promise.reject(error);
+        }
 
         if (error.response?.status === 401 && !originalRequest._retry) {
-          //   if (isRefreshing) {
-          //     // Just reject immediately if a refresh is already happening
-          //     return Promise.reject(error);
-          //   }
-
+          // prevent loop
           originalRequest._retry = true;
-          //   isRefreshing = true;
 
           try {
+            // make API call to the refresh token endpoint
             const response = await axios.post(
               "/token/refresh",
               {},
-              { withCredentials: true },
+              { withCredentials: true, _intercepted: true },
             );
-
             const { accessToken, userInfo, expiresAt } = response.data;
+            // set the state
             authContext.setAuthState({ accessToken, userInfo, expiresAt });
 
+            // if all is successful assign  Bearer token with the accessToken
             originalRequest.headers.Authorization = "Bearer " + accessToken;
             return axios(originalRequest);
           } catch (refreshError) {
-            // Optionally handle logout here if refresh fails
+            // if there is err reject the Promise and logout the user
+            authContext.logout();
             return Promise.reject(refreshError);
-          } finally {
-            // isRefreshing = false;
           }
         }
 
@@ -59,7 +61,7 @@ const AxiosInterceptor = ({ children }) => {
       axios.interceptors.request.eject(requestInterceptor);
       axios.interceptors.response.eject(responseInterceptor);
     };
-  }, [authContext.authState.accessToken]);
+  }, [authContext.authState.accessToken, authContext.logout]);
 
   return children;
 };
