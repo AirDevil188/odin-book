98b1425511ae2bab89f2a2667c9f9160ea77b2f4
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-18 00:27:16 +0200

    remove: redudant beforeLoad func

--- frontend/src/routes/__root.jsx
+++ frontend/src/routes/__root.jsx
@@ -1,26 +1,11 @@
 // __root.jsx
-import { createRootRoute, redirect } from "@tanstack/react-router";
+import { createRootRoute } from "@tanstack/react-router";
 import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import ErrorBoundary from "../components/Error Boundary/ErrorBoundary";
 import { ToastContainer } from "react-toastify";
-import axios from "../api/axiosInstance";
 
 export const Route = createRootRoute({
-  beforeLoad: async ({ context }) => {
-    const { setAuthState, authState } = context;
-    if (authState.accessToken) {
-      return;
-    }
-    const res = await axios.post("/token/refresh");
-
-    if (res.status === 200) {
-      const data = await res.data;
-      setAuthState(data);
-      return;
-    }
-    throw redirect({ to: "/sign-in" });
-  },
   component: () => {
     return (
       <>
