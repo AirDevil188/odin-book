6cfe6f7d90d547640c050d8d4019cf733c08c5a2
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-22 12:05:50 +0200

    add: layout component

---
+++ frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar/Navbar";
+import Sidebar from "../Sidebar/Sidebar";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+    </>
+  );
+};
+
+export default Layout;
